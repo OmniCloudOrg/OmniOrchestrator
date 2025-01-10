cae612dd4e2e43c5df983f8a924d9e7a99d8a07f
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-01-09 22:07:42 -0500

    Update user_create.sql

--- sql/versions/V1/queries/user/user_create.sql
+++ sql/versions/V1/queries/user/user_create.sql
@@ -1,1 +1,29 @@
-WITH new_user AS (SELECT CASE WHEN EXISTS (SELECT 1 FROM users WHERE email = ?) THEN RAISE(ROLLBACK, 'Error: Email already exists.') END) INSERT INTO users (email, name, password, active, last_login_at, created_at, updated_at) VALUES (?, ?, ?, ?, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+WITH new_user AS (
+    SELECT 
+        CASE 
+            WHEN EXISTS (
+                SELECT 1 
+                FROM users 
+                WHERE email = ?
+            ) 
+            THEN RAISE(ROLLBACK, 'Error: Email already exists.')
+        END
+) 
+INSERT INTO users (
+    email, 
+    name, 
+    password, 
+    active, 
+    last_login_at, 
+    created_at, 
+    updated_at
+) 
+VALUES (
+    ?, 
+    ?, 
+    ?, 
+    ?, 
+    NULL, 
+    CURRENT_TIMESTAMP, 
+    CURRENT_TIMESTAMP
+);
